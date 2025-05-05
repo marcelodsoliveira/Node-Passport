40076de998c2240506d81df9666568efe812849e
Marcelo Santos <oliveira.lnx@gmail.com>
2025-05-05 05:58:15 -0300

    Installed passport lib and configured in server.ts and passport.ts

---
+++ src/config/passport.ts
@@ -0,0 +1,6 @@
+import passport from "passport";
+
+// passport.use( ... ); // This is where you would configure your authentication strategy
+
+export default passport;
+// This file is used to configure the passport authentication strategy.
