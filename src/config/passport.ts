6dc1c390206858a9a3ac52e035fe731b9b3e828c
Marcelo Santos <oliveira.lnx@gmail.com>
2025-05-07 05:55:07 -0300

    Added passport logic in passport.ts

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,6 +1,18 @@
 import passport from "passport";
+import { BasicStrategy } from "passport-http";
+import { User } from "../models/User";
 
-// passport.use( ... ); // This is where you would configure your authentication strategy
+
+const noAuthorizedJson = { status: 401, message: "Não autorizado" };
+
+passport.use(new BasicStrategy(async (email, password, done) => {
+  if(email&& password){
+    const user = await User.findOne({
+      where: { email, password }
+    });
+  }
+  return done(noAuthorizedJson, false);
+}));
 
 export default passport;
 // This file is used to configure the passport authentication strategy.
