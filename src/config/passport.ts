ea0ae0cf31ec01e49c7d0b68d5b73348a3be7f8e
Marcelo Santos <oliveira.lnx@gmail.com>
2025-05-12 13:56:08 -0300

    Tests

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,18 +1,29 @@
 import passport from "passport";
 import { BasicStrategy } from "passport-http";
-import { User } from "../models/User";
+import { User, UserInstance } from "../models/User";
+import { NextFunction } from "express";
+import { RequestHandler } from "express"; 
 
 
-const noAuthorizedJson = { status: 401, message: "Não autorizado" };
+const notAuthorizedJson = { status: 401, message: "Não autorizado" };
 
 passport.use(new BasicStrategy(async (email, password, done) => {
-  if(email&& password){
+  if(email&& password) {
     const user = await User.findOne({
       where: { email, password }
     });
+    if(user) {
+      return done(null, user);
+    }
   }
-  return done(noAuthorizedJson, false);
+  return done(notAuthorizedJson, false);
 }));
+
+export const privateRoute: RequestHandler = (req, res, next) => {
+  passport.authenticate('basic', (err: any, user: any) => {
+    return user ? next() : next(notAuthorizedJson);
+  })(req, res, next);
+}
 
 export default passport;
 // This file is used to configure the passport authentication strategy.
