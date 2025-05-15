219b10344c590c57d5205a8ad3471278743990c3
Marcelo Santos <oliveira.lnx@gmail.com>
2025-05-15 06:09:13 -0300

    Final class about jwt token

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,6 +1,8 @@
+import { Request, Response, NextFunction } from "express";
 import passport from "passport";
 import dotenv from "dotenv";
 import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import jwt from "jsonwebtoken"; 
 import { User } from "../models/User";
 
 
@@ -23,7 +25,16 @@ passport.use(new JwtStrategy(options, async (payload, done) => {
   } 
 }));
 
+export const generateToken = (data: object) => {
+  return jwt.sign(data, process.env.JWT_SECRET as string);
+}
 
+export const privateRoute = (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate('jwt', (err:any, user:any) => {
+    req.user = user;
+    return user ? next() : next(notAuthorizedJson);
+  })(req, res, next);
+}
 
 export default passport;
 // This file is used to configure the passport authentication strategy.
--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { User } from '../models/User';
+import { generateToken } from '../config/passport';
 
 export const ping = (req: Request, res: Response) => {
     res.json({pong: true});
@@ -12,9 +13,10 @@ export const register = async (req: Request, res: Response) => {
         let hasUser = await User.findOne({where: { email }});
         if(!hasUser) {
             let newUser = await User.create({ email, password });
+            const token = generateToken({ id: newUser.id });
 
             res.status(201);
-            res.json({ id: newUser.id });
+            res.json({ id: newUser.id, token });
         } else {
             res.json({ error: 'E-mail já existe.' });
         }
@@ -24,7 +26,20 @@ export const register = async (req: Request, res: Response) => {
 }
 
 export const login = async (req: Request, res: Response) => {
-    res.json({ status: true });
+    if(req.body.email && req.body.password) {
+        let email: string = req.body.email;
+        let password: string = req.body.password;
+
+        let user = await User.findOne({ 
+            where: { email }
+        });
+
+        if(user) {
+            const token = generateToken({ id: user.id });
+            res.json({ status:true, token});
+            return;
+        }
+    }
 }
 
 export const list = async (req: Request, res: Response) => {
